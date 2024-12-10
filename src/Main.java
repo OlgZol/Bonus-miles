4548b1adda4252a2468c46b7408864124ee2959e
Olga Zolotova <olga_malashonkova@mail.ru>
2024-12-10 20:47:16 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+
+        int miles = price / 20;
+
+        System.out.println("Бонусных миль: " + miles);
+    }
+}
